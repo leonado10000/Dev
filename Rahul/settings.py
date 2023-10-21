7cefde584f376419b9069a3836c6afb12009bf17
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2023-10-21 20:55:31 +0530

    Update settings.py

--- Rahul/settings.py
+++ Rahul/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-+=$dwy-vf#wx=@y$-w^$%z=dp+z%p^nurm-o_t-rj(*zul2-1@"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['RahulJangra.vercel.app','.vercel.app','192.168.0.187','','*']
 
