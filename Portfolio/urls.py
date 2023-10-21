3aac5be4ef6c76d496ab5f1304066f2555ad8388
Rahul Jangra <106389897+leonado10000@users.noreply.github.com>
2023-10-22 00:25:08 +0530

    projects and navbar

--- Portfolio/urls.py
+++ Portfolio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 
 from . import views
 urlpatterns = [
-    path('', views.index),
-    path('projects', views.projects)
+    path('', views.index, name="ind"),
+    path('projects', views.projects, name="projects")
 ]
